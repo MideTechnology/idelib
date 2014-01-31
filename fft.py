6a67af4f20566ce4cef637e85275b0059fad477a
David Stokes <dstokes@mide.com>
2014-01-31 17:28:17 -0500

    Temp check-in of spectrogram experiments.

--- fft.py
+++ fft.py
@@ -9,6 +9,7 @@ Created on Dec 18, 2013
 
 from collections import Iterable
 import csv
+from itertools import izip
 
 import numpy as np
 from numpy.core import hstack
@@ -49,7 +50,8 @@ class FFTView(wx.Frame, MenuMixin):
         """
         kwargs.setdefault("title", self.FULLNAME)
         self.root = kwargs.pop("root", None)
-        self.sources = kwargs.pop("sources", None)
+        self.source = kwargs.pop("source", None)
+        self.subchannels = kwargs.pop("subchannels", None)
         self.range = (kwargs.pop("start",0), kwargs.pop("end",-1))
         self.data = kwargs.pop("data",None)
         self.sliceSize = kwargs.pop("sliceSize", 2**16)
@@ -75,22 +77,24 @@ class FFTView(wx.Frame, MenuMixin):
         self.Show(True)
         self.Update()
         
+        if self.source is None and self.subchannels is not None:
+            self.source = self.subchannels[0].parent.getSession(
+                                                    self.root.session.sessionId)
         
         self.draw()
 
 
     def draw(self):
         self.lines = None
-        if self.sources is not None:
-            channel = self.sources[0].parent.getSession(self.root.session.sessionId)
-            subchannelIds = [c.id for c in self.sources]
-            start, stop = channel.getRangeIndices(*self.range)
-            data = channel.itervalues(start, stop, subchannels=subchannelIds)
+        if self.subchannels is not None:
+            subchannelIds = [c.id for c in self.subchannels]
+            start, stop = self.source.getRangeIndices(*self.range)
+            data = self.source.itervalues(start, stop, subchannels=subchannelIds)
             # BUG: Calculation of actual sample rate is wrong. Investigate.
 #             fs = (channel[stop][-2]-channel[start][-2]) / ((stop-start) + 0.0)
-            fs = channel.getSampleRate()
-            self.data = self.generateFFTData(data, stop-start, 
-                                             len(self.sources), fs, 
+            fs = self.source.getSampleRate()
+            self.data = self.generateData(data, stop-start, 
+                                             len(self.subchannels), fs, 
                                              self.sliceSize)
             
         if self.data is not None:
@@ -210,15 +214,15 @@ class FFTView(wx.Frame, MenuMixin):
 
         for i in range(cols):
             points = (hstack((freqs, self.data[:,i+1].reshape(-1,1))))
-            name = self.sources[i-1].name
+            name = self.subchannels[i-1].name
 
-            lines.append(P.PolyLine(points, legend=name, colour=self.root.getPlotColor(self.sources[i-1])))#colors[i]))
+            lines.append(P.PolyLine(points, legend=name, colour=self.root.getPlotColor(self.subchannels[i-1])))#colors[i]))
             
         self.lines = P.PlotGraphics(lines, title=self.GetTitle(), 
                                     xLabel="Frequency", yLabel="Amplitude")
         
     
-    
+    @classmethod
     def from2diter(self, data, rows=None, cols=1):
         """ Build a 2D `numpy.ndarray` from an iterator (e.g. what's produced by 
             `EventList.itervalues`). 
@@ -247,8 +251,8 @@ class FFTView(wx.Frame, MenuMixin):
         return points
     
     
-    def generateFFTData(self, data, rows=None, cols=1, fs=5000, 
-                        sliceSize=2**16):
+    @classmethod
+    def generateData(cls, data, rows=None, cols=1, fs=5000, sliceSize=2**16):
         """ Compute 1D FFT from one or more channels of data, using Welch's
             method.
         
@@ -269,7 +273,7 @@ class FFTView(wx.Frame, MenuMixin):
                 frequency.
         """
         
-        points = self.from2diter(data, rows, cols)
+        points = cls.from2diter(data, rows, cols)
         rows, cols = points.shape
         shape = points.shape
         points.resize((max(nextPow2(shape[0]),sliceSize), shape[1]))
@@ -306,6 +310,8 @@ class FFTView(wx.Frame, MenuMixin):
 #===============================================================================
 # 
 #===============================================================================
+import colorsys
+import Image
 
 class SpectrogramView(FFTView):
     """
@@ -313,20 +319,182 @@ class SpectrogramView(FFTView):
     NAME = "Spectrogram"
     FULLNAME = "Spectrogram View"
     
+    @classmethod
+    def getColorFromNorm(self, n):
+        try:
+#             return tuple(map(lambda x: int(x*255), colorsys.hsv_to_rgb(n*.835,1,1)))
+            return tuple(map(lambda x: int(x*255), colorsys.hsv_to_rgb(n,1.0,1.0)))
+        except ValueError:
+            print "problem with color for %s", n
+            return (0,0,0)
+#         b = int(255*n)
+#         return b,b,b
+    
+    @classmethod
+    def makeLineList(self, data):
+        """
+        """
+        minAmp = min([np.amin(np.log10(d[0])) for d in data])
+        maxAmp = max([np.amax(np.log10(d[0])) for d in data])
+        
+        # Create a mapped function to normalize a numpy.ndarray
+        norm = np.vectorize(lambda x: (x-minAmp)/(maxAmp-minAmp))
+        
+        images = []
+        # XXX: THIS IS AN UGLY HACK AND IT WANTS TO DIE
+        fooimg = wx.Image('ssx.ico').Copy()
+        b = fooimg.GetDataBuffer()
+        for i in xrange(len(b)):
+            b[i]=chr(0)
+        fooimg.Rescale(data[0][0].shape[1],data[0][0].shape[0])
+        for amps, _freqs, _bins in data:
+            img = fooimg.Copy()#wx.EmptyBitmap(*data[0][0].shape)
+            buf = img.GetDataBuffer()
+            print img.HasAlpha()
+#             abuf = img.GetAlphaBuffer()
+            idx = 0
+            for p in norm(np.log10(amps)).reshape((1,-1)):
+                color = self.getColorFromNorm(p[0])
+                buf[idx]=chr(color[0])
+                buf[idx+1]=chr(color[1])
+                buf[idx+2]=chr(color[2])
+#                 abuf[idx/3]=chr(255)
+                if idx == 0:
+                    print color, map(repr, [buf[0], buf[1], buf[2]])
+                idx += 3
+            images.append(img)
+            
+        return images
+                
+   
+    @classmethod
+    def dumpImg(self, data):
+        """
+        """
+        minAmp = min([np.amin(np.log10(d[0])) for d in data])
+        maxAmp = max([np.amax(np.log10(d[0])) for d in data])
+        
+        print "minAmp:",minAmp," maxAmp:",maxAmp
+        for d in data:
+            print "mean:", d[0].mean()
+        # Create a mapped function to normalize a numpy.ndarray
+        norm = np.vectorize(lambda x: 255*((x-minAmp)/(maxAmp-minAmp)))
+        imgsize = data[0][0].shape[1], data[0][0].shape[0]
+        images = []
+        for amps, _freqs, _bins in data:
+            img = Image.new("L", imgsize, 0)
+            img.putdata(norm(np.log10(amps)).reshape((1,-1))[0,:])
+            images.append(img.transpose(Image.FLIP_TOP_BOTTOM))
+            
+        return images
+   
+
+    @classmethod
+    def dumpImg2(self, data):
+        """
+        """
+        mins = []
+        maxes = []
+        means = []
+        for d in data:
+            dlog = 10.0*np.log10(np.abs(d[0]))
+#             dlog = dlog - np.mean(dlog)
+            mins.append(np.amin(dlog))
+            maxes.append(np.amax(dlog))
+            means.append(np.mean(dlog))
+        minAmp = min(mins)
+        maxAmp = max(maxes)
+        meanAmp = np.mean(means)
+        
+        print "minAmp:",minAmp," maxAmp:",maxAmp," meanAmp:",meanAmp
+#         minAmp = meanAmp
+        
+        for d in data:
+            print "mean:", d[0].mean()
+        # Create a mapped function to normalize a numpy.ndarray
+#         norm = np.vectorize(lambda x: max(0,255*((x-minAmp)/(maxAmp-minAmp))))
+        norm = np.vectorize(lambda x: 255*((x-minAmp)/(maxAmp-minAmp)))
+        imgsize = data[0][0].shape[1], data[0][0].shape[0]
+        images = []
+        for amps, _freqs, _bins in data:
+            img = Image.new("L", imgsize, 0)
+            img.putdata(norm(10.0*np.log10(np.abs(amps))).reshape((1,-1))[0,:])
+            images.append(img.transpose(Image.FLIP_TOP_BOTTOM))
+            
+        return images
+   
+
+    @classmethod
+    def dumpColorImg(self, data):
+        """
+        """
+#         minAmp = min([np.amin(np.log10(d[0])) for d in data])
+#         maxAmp = max([np.amax(np.log10(d[0])) for d in data])
+        
+        mins = []
+        maxes = []
+        means = []
+        for d in data:
+            dlog = np.log10(d[0])
+            dlog = dlog - np.mean(dlog)
+            mins.append(np.amin(dlog))
+            maxes.append(np.amax(dlog))
+            means.append(np.mean(dlog))
+        minAmp = min(mins)
+        maxAmp = max(maxes)
+        meanAmp = np.mean(means)
+        
+        print "minAmp:",minAmp," maxAmp:",maxAmp, " meanAmp:", meanAmp
+
+        # Create a mapped function to normalize a numpy.ndarray
+        norm = np.vectorize(lambda x: ((x-minAmp)/(maxAmp-minAmp)))
+        imgsize = data[0][0].shape[1], data[0][0].shape[0]
+        images = []
+        for amps, _freqs, _bins in data:
+            buf = []
+            img = Image.new("RGB", imgsize, 0)
+            for val in norm(np.log10(amps).reshape((1,-1)))[0,:]:
+                buf.append(self.getColorFromNorm(val))
+            img.putdata(buf)
+            images.append(img.transpose(Image.FLIP_TOP_BOTTOM))
+            
+        return images
+
     
     def draw(self):
         """
         """
         # self.canvas is the plot canvas
-        pass
+        self.lines = None
+        if self.subchannels is not None:
+            subchannelIds = [c.id for c in self.subchannels]
+            start, stop = self.source.getRangeIndices(*self.range)
+            data = self.source.itervalues(start, stop, subchannels=subchannelIds)
+            # BUG: Calculation of actual sample rate is wrong. Investigate.
+#             fs = (channel[stop][-2]-channel[start][-2]) / ((stop-start) + 0.0)
+            fs = self.source.getSampleRate()
+            self.data = self.generateData(data, rows=stop-start, 
+                                          cols=len(self.subchannels), fs=fs, 
+                                          sliceSize=self.sliceSize,
+                                          slicesPerSec=4.0)
+        
+        # XXX: THESE LINES NEED TO BE REWRITTEN
+#         if self.data is not None:
+#             self.makeLineList()
+# 
+#         if self.lines is not None:
+#             self.canvas.Draw(self.lines)
+
+        self.SetCursor(wx.StockCursor(wx.CURSOR_DEFAULT))
 
     
     def OnHelp(self, evt):
         self.root.ask("Spectrogram Help not implemented!", "TODO:", style=wx.OK, parent=self)
 
 
-    def generateSpecData(self, data, rows=None, cols=1, fs=5000, 
-                        sliceSize=2**16):
+    @classmethod
+    def generateData(cls, data, rows=None, cols=1, fs=5000, sliceSize=2**16, 
+                     slicesPerSec=4.0, recordingTime=None):
         """ Compute 2D FFT from one or more channels of data.
         
             @note: This is the implementation from the old viewer and does not
@@ -345,5 +513,38 @@ class SpectrogramView(FFTView):
             @return: A multidimensional array, with the first column the 
                 frequency.
         """
-        pass
+#               self.myplot.specgram(theData[:,i], NFFT=self.specgram_nfft, Fs=fs, Fc=0, detrend=mlab.detrend_none,
+#                   window=mlab.window_hanning, noverlap=self.specgram_nfft/2,
+#                   cmap=cm.spectral, xextent=None, pad_to=None, sides='onesided',
+#                   scale_by_freq=None, figure=self.figure)
+    
+        points = cls.from2diter(data, rows, cols)
+        rows, cols = points.shape
+#         points.resize((max(nextPow2(rows),sliceSize), cols))
+#         recordingTime = (points[-1,0] - points[0,0]) * timeScalar
         
+        specgram_nfft = int(rows/(recordingTime*slicesPerSec))
+        
+        print "recordingTime:",recordingTime
+        print "specgram_nfft:",specgram_nfft
+        specData = []
+                
+        for i in xrange(cols):
+            pts = points[:,i]
+            # This is basically what matplotlib.mlab.specgram does
+            Pxx, freqs, bins = spec._spectral_helper(pts, pts, 
+                NFFT=specgram_nfft, Fs=fs, detrend=spec.detrend_none, 
+                window=spec.window_hanning, noverlap=specgram_nfft/2, 
+                pad_to=None, sides='onesided', scale_by_freq=None)
+            Pxx = Pxx.real
+    
+            Z = 10. * np.log10(Pxx)
+            Z = np.flipud(Z)
+
+            xmin, xmax = 0, np.amax(bins)
+        
+            specData.append((Pxx, freqs, bins))
+            
+        return specData
+        
+
