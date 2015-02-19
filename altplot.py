e109c8cde29d45ba4fd2c4abfd09ec13a224bc22
David Stokes <dstokes@mide.com>
2015-02-19 18:10:13 -0500

    Experiments resumed: pressure-as-altitude plot, start of migration of all preferences to same module.

--- altplot.py
+++ altplot.py
@@ -21,42 +21,45 @@ class MPL3115AltitudeParser(MPL3115PressureTempParser):
         """ Special-case parsing of a temperature data block.
         """
         sealevel = self.DEFAULT_SEALEVEL
-        M = 0.0289644 # [kg/mol] molar mass of Earth's air
-        g = 9.80665   # [m/s^2] gravitational acceleration constant
-        R = 8.31432   # [(N*m)/(mol*k)] universal gas constant
-        print type(self)
+#         M = 0.0289644 # [kg/mol] molar mass of Earth's air
+#         g = 9.80665   # [m/s^2] gravitational acceleration constant
+#         R = 8.31432   # [(N*m)/(mol*k)] universal gas constant
         press, t = super(MPL3115AltitudeParser, self).unpack_from(data, offset)
         
         if ((sealevel/press) < 4.47704808656731):
             L_b = -0.0065 # [K/m] temperature lapse rate
             h_b = 0.0  # [m] height above sea level (differing altitudes have differing time lapse rates
-            foo = math.pow((press/sealevel), (R*L_b)/(g*M))
+#             foo = math.pow((press/sealevel), (R*L_b)/(g*M))
+#             return (h_b+((t*((1.0/foo)-1.0))/L_b),)
+            foo = math.pow((press/sealevel), -1.8658449683059204)
             return (h_b+((t*((1.0/foo)-1.0))/L_b),)
         
         elif ((sealevel / press) < (18.507221149648668)):
+            T_2 = t - 71.5
 #             h_b = 11000
-#             T_2 = t - 71.5
 #             h_2 = (R*T_2*(math.log(press/sealevel)))/((-g)*M)
 #             p_c = 101325
 #             P_1 = 22632.1
 #             h_1 = ((R*T_2*(math.log(p_c/P_1)))/((-g)*M))+h_b
-            T_2 = t - 71.5
-            h_2 = (R*T_2*(math.log(press/sealevel)))/(-g*M)
-            h_1 = ((R*T_2*(math.log(101325/22632.1)))/(-g*M))+11000
+#             h_2 = (R*T_2*(math.log(press/sealevel)))/(-g*M)
+#             h_1 = ((R*T_2*(math.log(101325/22632.1)))/(-g*M))+11000
+            h_2 = (8.31432*T_2*(math.log(press/sealevel)))/-338.5759760257419
+            h_1 = ((T_2*12.462865699354536)/-338.5759760257419)+11000
             return (h_1+h_2,)
         
         return (0,) # Is this okay?
 
 
-def addAltPlot(doc, sessionId=None):
+def addAltChannel(doc, chId=128, subChId=0, sessionId=None):
     """ Create a new sensor channel using the altitude parser, which uses the
         existing Channel 1 data.
         
         For some reason, the copy isn't getting the _data.
     """
-    ch = doc.sensors[0].addChannel(name="Computed Altitude", channelId=128, parser=MPL3115AltitudeParser(),
+    ch = doc.sensors[0].addChannel(name="Computed Altitude", channelId=chId, 
+                                   parser=MPL3115AltitudeParser(),
                                    cache=True, singleSample=True)
-    ch.addSubChannel(0, name="Altitude", units=("Altitude","m"),
+    ch.addSubChannel(subChId, name="Altitude", units=("Altitude","m"),
                      displayRange=(0,20000))
     
     sourceEl = doc.channels[1].getSession(sessionId)
@@ -64,12 +67,17 @@ def addAltPlot(doc, sessionId=None):
     el._data = sourceEl._data
     ch.sessions[sessionId] = el
     return el
-#         for d in self.dataset.getPlots(debug=self.showDebugChannels):
-#             el = d.getSession(self.session.sessionId)
-#             p = self.plotarea.addPlot(el, title=d.name)
-#             if meanSpan is not None:
-#                 p.removeMean(True, meanSpan)
-#         
-#         self.enableChildren(True)
-#         # enabling plot-specific menu items happens on page select; do manually
-#         self.plotarea.getActivePage().enableMenus()
+
+
+def addAltPlot(view, chId=128, subChId=0):
+    if chId not in view.dataset.channels:
+        el = addAltChannel(view.dataset, chId, subChId)
+        p = view.plotarea.addPlot(el, el.parent.name)
+        for d in view.dataset.getPlots(debug=view.showDebugChannels):
+            el = d.getSession(view.session.sessionId)
+            p = view.plotarea.addPlot(el, title=d.name)
+         
+        view.enableChildren(True)
+        # enabling plot-specific menu items happens on page select; do manually
+        view.plotarea.getActivePage().enableMenus()
+    
--- preference_dialog.py
+++ preference_dialog.py
@@ -6,6 +6,9 @@ Created on May 8, 2014
 @author: dstokes
 '''
 
+import fnmatch
+import json
+import os.path
 import time
 
 import wx; wx=wx
@@ -13,7 +16,284 @@ import wx.propgrid as PG
 import wx.lib.sized_controls as SC
 
 from updater import INTERVALS
+from build_info import VERSION, DEBUG
+from logger import logger
 
+#===============================================================================
+# 
+#===============================================================================
+
+class Preferences(object):
+    
+    # Preferences format version: change if a change renders old ones unusable.
+    PREFS_VERSION = 0
+    defaultPrefsFile = 'ss_lab.cfg'
+    
+    # Default settings. Any user-changed preferences override these.
+    defaultPrefs = {
+        'defaultFilename': '', #'data.dat',
+        'fileHistory': {},
+        'fileHistorySize': 10,
+        
+        # Precision display of numbers
+        'precisionX': 4,
+        'precisionY': 4,
+        
+        # Data modifications
+        'removeMean': True,
+        'removeRollingMean': False,
+        'rollingMeanSpan': 5.0, # In seconds
+        
+        # Rendering
+        'antialiasing': False,
+        'antialiasingMultiplier': 3.33,
+        'resamplingJitter': False,
+        'resamplingJitterAmount': 0.125,
+        'drawMajorHLines': True,
+        'drawMinorHLines': True, #False,
+        'drawMinMax': False,
+        'drawMean': False,
+        'drawPoints': True,
+        'plotLineWidth': 1,
+        'originHLineColor': wx.Colour(200,200,200),
+        'majorHLineColor': wx.Colour(240,240,240),
+        'minorHLineColor': wx.Colour(240,240,240),
+        'minRangeColor': wx.Colour(190,190,255),
+        'maxRangeColor': wx.Colour(255,190,190),
+        'meanRangeColor': wx.Colour(255,255,150),
+        'plotBgColor': wx.Colour(255,255,255),
+        # Plot colors, stored by channel:subchannel IDs.
+        'plotColors': {"00.0": "BLUE",
+                       "00.1": "GREEN",
+                       "00.2": "RED",
+                       "01.0": "DARK GREEN",
+                       "01.1": "VIOLET"
+        },
+        # default colors: used for subchannel plots not in plotColors
+        'defaultColors': ["DARK GREEN",
+                          "VIOLET",
+                          "GREY",
+                          "YELLOW",
+                          "MAGENTA",
+                          "NAVY",
+                          "PINK",
+                          "SKY BLUE",
+                          "BROWN",
+                          "CYAN",
+                          "DARK GREY",
+                          "GOLD",
+                          "BLACK",
+                          "BLUE VIOLET"],
+
+#         'locale': 'English_United States.1252', # Python's locale name string
+        'locale': 'LANGUAGE_ENGLISH_US', # wxPython constant name (wx.*)
+        'loader': dict(numUpdates=100, updateInterval=1.0),
+        'openOnStart': True,
+        'showDebugChannels': DEBUG,
+        'showFullPath': True,#False,
+        'showUtcTime': True,
+        'titleLength': 80,
+
+        # Automatic update checking
+        'updater.interval': 3, # see updater.INTERVALS
+        'updater.lastCheck': 0, # Unix timestamp of the last version check
+        'updater.version': VERSION, # The last version check
+
+        # WVR/SSX-specific parameters: the hard-coded warning range.        
+        'wvr.tempMin': -20.0,
+        'wvr.tempMax': 60.0,
+    }
+
+
+    def __init__(self, filename=None):
+        self.prefsFile = filename or self.defaultPrefsFile
+        self.loadPrefs()
+
+
+    def loadPrefs(self, filename=None):
+        """ Load saved preferences from file.
+        """
+        def tuple2color(c):
+            if isinstance(c, list):
+                return wx.Colour(*c)
+            return c
+        
+#         self.fileHistory = wx.FileHistory()
+        filename = filename or self.prefsFile
+        if not filename:
+            return {}
+        
+        filename = os.path.realpath(os.path.expanduser(filename))
+        logger.debug(u"Loading preferences from %r" % filename)
+
+        prefs = {}
+        if not os.path.exists(filename):
+            # No preferences file; probably the first run for this machine/user
+            return {}
+        try:
+            with open(filename) as f:
+                prefs = json.load(f)
+                if isinstance(prefs, dict):
+                    vers = prefs.get('prefsVersion', self.PREFS_VERSION)
+                    if vers != self.PREFS_VERSION:
+                        # Mismatched preferences version!
+                        # FUTURE: Possibly translate old prefs to new format
+                        n = "n older" if vers < self.PREFS_VERSION else " newer"
+                        wx.MessageBox("The preferences file appears to use a%s "
+                            "format than expected;\ndefaults will be used." % n,
+                            "Preferences Version Mismatch")
+                        return {}
+                    # De-serialize *Color attributes (single colors)
+                    for k in fnmatch.filter(prefs.keys(), "*Color"):
+                        prefs[k] = tuple2color(prefs[k])
+                    # De-serialize *Colors attributes (lists of colors)
+                    for k in fnmatch.filter(prefs.keys(), "*Colors"):
+                        if isinstance(prefs[k], list):
+                            for i in xrange(len(prefs[k])):
+                                prefs[k][i] = tuple2color(prefs[k][i])
+        except (ValueError, IOError):# as err:
+            # Import problem. Bad file will raise IOError; bad JSON, ValueError.
+            wx.MessageBox("An error occurred while trying to read the "
+                          "preferences file.\nDefault settings will be used.",
+                          "Preferences File Error")
+            return {}
+        
+        # Load recent file history
+#         hist = prefs.setdefault('fileHistory', {}).setdefault('import', [])
+#         map(self.fileHistory.AddFileToHistory, hist)
+#         self.fileHistory.UseMenu(self.recentFilesMenu)
+#         self.fileHistory.AddFilesToMenu()
+            
+        return prefs
+
+
+    def savePrefs(self, filename=None):
+        """ Write custom preferences to a file.
+        """
+        def _fix(d):
+            if isinstance(d, (list,tuple)):
+                d = [_fix(x) for x in d]
+            elif isinstance(d, dict):
+                for k,v in d.iteritems():
+                    d[k] = _fix(v)
+            elif isinstance(d, wx.Colour):
+                d = tuple(d)
+            return d
+        
+        prefs = self.prefs.copy()
+        prefs['prefsVersion'] = self.PREFS_VERSION
+        filename = filename or self.prefsFile
+        
+        try:
+            path = os.path.split(filename)[0]
+            if not os.path.exists(path):
+                os.makedirs(path)
+            with open(filename, 'w') as f:
+                json.dump(_fix(prefs), f, indent=2, sort_keys=True)
+        except IOError:# as err:
+            # TODO: Report a problem, or just ignore?
+            pass
+        
+    
+    def saveAllPrefs(self, filename=None, hideFile=None):
+        """ Save all preferences, including defaults, to the config file.
+            Primarily for debugging.
+        """
+        prefs = self.defaultPrefs.copy()
+        prefs.update(self.prefs)
+        self.prefs = prefs
+        self.savePrefs(filename, hideFile)
+
+    
+    def addRecentFile(self, filename, category="import"):
+        """ Add a file to a history list. If the list is at capacity, the
+            oldest file is removed.
+        """
+        self.changedFiles = True
+        allFiles = self.prefs.setdefault('fileHistory', {})
+        files = allFiles.setdefault(category, [])
+        if filename:
+            if filename in files:
+                files.remove(filename)
+            files.insert(0,filename)
+        allFiles[category] = files[:(self.getPref('fileHistorySize'))]
+
+
+    def getRecentFiles(self, category="import"):
+        """ Retrieve the list of recent files within a category.
+        """
+        hist = self.prefs.setdefault('fileHistory', {})
+        return hist.setdefault(category, [])
+
+
+    def getPref(self, name, default=None, section=None):
+        """ Retrieve a value from the preferences.
+            @param prefName: The name of the preference to retrieve.
+            @keyword default: An optional default value to return if the
+                preference is not found.
+            @keyword section: An optional "section" name from which to
+                delete. Currently a prefix in this implementation.
+        """
+        if section is not None:
+            name = "%s.%s" % (section, name)
+        return self.prefs.get(name, self.defaultPrefs.get(name, default))
+
+
+    def setPref(self, name, val, section=None, persistent=True):
+        """ Set the value of a preference. Returns the value set as a
+            convenience.
+        """
+        if section is not None:
+            name = "%s.%s" % (section, name)
+        prefs = self.prefs if persistent else self.defaultPrefs
+        prefs[name] = val
+        return val
+
+
+    def hasPref(self, name, section=None, defaults=False):
+        """ Check to see if a preference exists, in either the user-defined
+            preferences or the defaults.
+        """
+        if section is not None:
+            name = "%s.%s" % (section, name)
+        if defaults:
+            return (name in self.prefs) or (name in self.defaultPrefs)
+        return name in self.prefs
+    
+    
+    def deletePref(self, name=None, section=None):
+        """ Delete one or more preferences. Glob-style wildcards are allowed.
+        
+            @keyword name: The name of the preference to delete. Optional if
+                `section` is supplied
+            @keyword section: An optional section name, limiting the scope.
+            @return: The number of deleted preferences.
+        """
+        if section is not None:
+            name = name if name is not None else "*"
+            name = "%s.%s" % (section, name)
+        if name is None:
+            return
+        keys = fnmatch.filter(self.prefs.keys(), name)
+        for k in keys:
+            self.prefs.pop(k, None)
+        return len(keys)
+
+
+    def editPrefs(self, evt=None):
+        """ Launch the Preferences editor.
+            
+            @param evt: Unused; a placeholder to allow this method to be used
+                as an event handler.
+        """
+        newPrefs = PrefsDialog.editPrefs(None, self.prefs, self.defaultPrefs)
+        if newPrefs is not None:
+            self.prefs = newPrefs
+            self.savePrefs()
+            
+            for v in self.viewers:
+                v.loadPrefs()
+    
 #===============================================================================
 # 
 #===============================================================================
