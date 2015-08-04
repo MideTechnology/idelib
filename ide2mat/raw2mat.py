3e6b2ef5f4fde453f7347b637750158518054331
David Stokes <dstokes@mide.com>
2015-08-04 16:11:14 -0400

    Added channel selection, additional updates for new firmware recordings.

--- ide2mat/raw2mat.py
+++ ide2mat/raw2mat.py
@@ -9,7 +9,9 @@ from itertools import izip
 import importlib
 import locale
 import os.path
+import platform
 import sys
+import tempfile
 import time
 
 import numpy as np
@@ -29,21 +31,30 @@ from mide_ebml.parsers import MPL3115PressureTempParser, ChannelDataBlock
 
 from devices import SlamStickX
 
-from build_info import DEBUG, BUILD_NUMBER, VERSION, BUILD_TIME
+from build_info import DEBUG, BUILD_NUMBER, VERSION, BUILD_TIME #@UnusedImport
 __version__ = VERSION
 
 #===============================================================================
 # 
 #===============================================================================
 
+class MATExportError(Exception):
+    pass
+
+#===============================================================================
+# 
+#===============================================================================
+
 class AccelDumper(object):
-    """ Parser replacement that dumps accelerometer data as read. """
+    """ Parser replacement that dumps accelerometer data as read. 
+    """
     maxTimestamp = ChannelDataBlock.maxTimestamp
     timeScalar = 1000000.0 / 2**15
     
-    def __init__(self, numCh, writer=None, startTime=0, endTime=None):
+    def __init__(self, source, writer=None, startTime=0, endTime=None):
         self.writer = writer
-        self.numCh = numCh
+        self.source = source
+        self.numCh = len(source.subchannels)
         self.numRows = 0
         self.numSamp = 0
         self.firstTime = None
@@ -86,7 +97,7 @@ class AccelDumper(object):
             raise StopIteration
         
         data = el.value[-1].value 
-        vals= np.frombuffer(data, np.uint16).reshape((-1,3))
+        vals= np.frombuffer(data, np.uint16).reshape((-1,self.numCh))
         self.numRows += vals.shape[0]
         self.numSamp += vals.shape[0] * self.numCh
         times = np.linspace(blockStart, blockEnd, vals.shape[0]) * self.timeScalar
@@ -100,10 +111,11 @@ class MPL3115Dumper(AccelDumper):
     """ Parser replacement that dumps temperature/pressure data as read.
         This accumulates data rather than writing it directly.
     """
-    tempParser = MPL3115PressureTempParser()
     
     def __init__(self, *args, **kwargs):
         super(MPL3115Dumper, self).__init__(*args, **kwargs)
+        self.unpacker = self.source.parser.unpack_from
+        print self.source.parser
         self.data = []
     
     def write(self, el):
@@ -116,7 +128,7 @@ class MPL3115Dumper(AccelDumper):
         
         self.numRows += 1
         self.numSamp += self.numCh
-        self.data.append(self.tempParser.unpack_from(el.value[-1].value))
+        self.data.append(self.unpacker(el.value[-1].value))
         return True
 
 #===============================================================================
@@ -172,7 +184,7 @@ class SimpleUpdater(object):
         else:
             if percent is not None:
                 num = locale.format("%d", count, grouping=True)
-                msg = "%s samples exported" % num
+                msg = "%s samples read" % num
                 if msg != self.lastMsg:
                     self.lastMsg = msg
                     msg = "%s (%s)" % (msg, self.formatter % (percent*100))
@@ -188,15 +200,54 @@ class SimpleUpdater(object):
             sys.stdout.flush()
 
 
-    
 #===============================================================================
 # 
 #===============================================================================
 
-def raw2mat(ideFilename, matFilename=None, channelId=0, calChannelId=1, 
-            dtype="double", nocal=False, raw=False, accelOnly=True,
-            noTimes=False, startTime=0, endTime=None, 
-            maxSize=matfile.MatStream.MAX_SIZE, updateInterval=1.5, **kwargs):
+class TempWriter(object):
+    """ Thing to write data to a temporary file.
+    """
+    def __init__(self, stream):
+        self.stream = stream
+    
+    def writerow(self, r):
+        self.stream.write('%d, %r\n' % (r[0], list(r[1])))
+
+#===============================================================================
+# 
+#===============================================================================
+
+def showInfo(ideFilename, **kwargs):
+    """
+    """
+    print ideFilename
+    print "=" * 70
+    with open(ideFilename, 'rb') as stream:
+        doc = importer.openFile(stream, **kwargs)
+        print "Sensors"
+        print "-" * 40
+        for s in sorted(doc.sensors.values()):
+            print "  Sensor %d: %s" % (s.id, s.name)
+            if s.traceData:
+                for i in s.traceData.items():
+                    print "    %s: %s" % i
+        print 
+        print "Channels"
+        print "-" * 40
+        for c in sorted(doc.channels.values()):
+            print "  Channel %d: %s" % (c.id, c.displayName)
+            for sc in c.subchannels:
+                print "    Subchannel %d.%d: %s" % (c.id, sc.id, sc.displayName)
+    print "=" * 70
+
+
+#===============================================================================
+# 
+#===============================================================================
+
+def raw2mat(ideFilename, matFilename=None, dtype="double", channels=None,
+            noTimes=False, startTime=0, endTime=None, updateInterval=1.5,  
+            maxSize=matfile.MatStream.MAX_SIZE, **kwargs):
     """ The main function that handles generating MAT files from an IDE file.
     """
     
@@ -219,20 +270,39 @@ def raw2mat(ideFilename, matFilename=None, channelId=0, calChannelId=1,
         pressTempCh = ssx.getTempChannel().parent
         pressTempChId = pressTempCh.id
         
+        dcAccelCh = ssx.getAccelChannel(dc=True)
+        dcAccelChId = dcAccelCh.id if dcAccelCh is not None else None
+        
         numAccelCh = len(accelCh.subchannels)
         numTempCh = len(pressTempCh.subchannels)
+        numDcAccelCh = 0 if dcAccelCh is None else len(dcAccelCh.subchannels)
         
         totalSize = os.path.getsize(ideFilename) + 0.0
         nextUpdate = time.time() + updateInterval
         
+        if channels is None:
+            channels = doc.channels.keys()
+        else:
+            missing = [str(c) for c in channels if c not in doc.channels]
+            if missing:
+                raise MATExportError("Unknown channel(s): %s" % (', '.join(missing)))
+        
         try:
-            mat.startArray(accelCh.name, numAccelCh, dtype=MP.miUINT16, noTimes=True, colNames=[c.name for c in accelCh.subchannels])
+            mat.startArray(accelCh.name, numAccelCh, dtype=MP.miUINT16, 
+                           noTimes=True, colNames=[c.displayName for c in accelCh.subchannels])
     
-#             dumpers = (AccelDumper(3, mat.writeRow, startTime, endTime), 
-#                        MPL3115Dumper(2, None, startTime, endTime))
-            dumpers = {accelChId: AccelDumper(numAccelCh, mat.writeRow, startTime, endTime), 
-                       pressTempChId: MPL3115Dumper(numTempCh, None, startTime, endTime)}
+#             dumpers = {accelChId: AccelDumper(accelCh, mat.writeRow, startTime, endTime), 
+#                        pressTempChId: MPL3115Dumper(pressTempCh, None, startTime, endTime)}
+            dumpers = {}
 
+            if accelChId in channels:
+                dumpers[accelChId] = AccelDumper(accelCh, mat.writeRow, startTime, endTime)
+            if pressTempChId in channels:
+                dumpers[pressTempChId] = MPL3115Dumper(pressTempCh, None, startTime, endTime)
+            if dcAccelChId in channels:
+                tempFile = open(os.path.join(tempfile.gettempdir(), 'ch%d_temp.csv' % dcAccelChId), 'wb')
+                tempWriter = TempWriter(tempFile) #csv.writer(tempFile)
+                dumpers[dcAccelChId] = AccelDumper(dcAccelCh, tempWriter.writerow, startTime, endTime)
                     
             lastMat = ''
             writeMsg = '' 
@@ -275,12 +345,22 @@ def raw2mat(ideFilename, matFilename=None, channelId=0, calChannelId=1,
                 
             mat.endArray()
             
-            if not accelOnly:
+            if pressTempChId in channels:
                 d = dumpers[pressTempChId]
-                mat.startArray(pressTempCh.name, numTempCh,
-                       dtype=MP.miSINGLE, noTimes=True)
+                mat.startArray(pressTempCh.name, numTempCh, dtype=MP.miSINGLE, 
+                               noTimes=True, colNames=[c.displayName for c in pressTempCh.subchannels])
                 for r in izip(np.linspace(d.firstTime, d.lastTime, len(d.data)), d.data):
                     mat.writeRow(r)
+                mat.endArray()
+
+            if dcAccelCh is not None and dcAccelChId in channels:
+                tempFile.close()
+                d = dumpers[dcAccelChId]
+                mat.startArray(dcAccelCh.name, numDcAccelCh, dtype=MP.miUINT16, 
+                               noTimes=True, colNames=[c.displayName for c in dcAccelCh.subchannels])
+                with open(tempFile.name, 'rb') as f:
+                    for r in f:
+                        mat.writeRow(eval(r))
                 mat.endArray()
 
             # Calculate actual sampling rate based on total count and total time
@@ -311,17 +391,18 @@ if __name__ == "__main__":
     
     argparser = argparse.ArgumentParser(description="Mide Raw .IDE to .MAT Converter v%d.%d.%d - Copyright (c) %d Mide Technology" % (VERSION+(datetime.now().year,)))
     argparser.add_argument('-o', '--output', help="The output path to which to save the .MAT files. Defaults to the same as the source file.")
-    argparser.add_argument('-a', '--accelOnly', action='store_true', help="Export only accelerometer data.")
+    argparser.add_argument('-c', '--channel', action='append', type=int, help="Export the specific channel. Can be used multiple times. If not used, all channels will export.")
     argparser.add_argument('-m', '--maxSize', type=int, default=matfile.MatStream.MAX_SIZE, help="The maximum MAT file size in bytes. Must be less than 2GB.")
     argparser.add_argument('-t', '--startTime', type=float, help="The start of the time span to export (seconds from the beginning of the recording).", default=0)
     argparser.add_argument('-e', '--endTime', type=float, help="The end of the time span to export (seconds from the beginning of the recording).")
     argparser.add_argument('-d', '--duration', type=float, help="The length of time to export, relative to the --startTime. Overrides the specified --endTime")
+    argparser.add_argument('-i', '--info', action='store_true', help="Show information about the file and exit.")
     argparser.add_argument('-v', '--version', action='store_true', help="Show detailed version information and exit.")
     argparser.add_argument('source', nargs="*", help="The source .IDE file(s) to convert.")
 
     args = argparser.parse_args()
+    
     if args.version is True:
-        import platform
         print argparser.description
         print "Converter version %d.%d.%d (build %d) %s, %s" % (VERSION + (BUILD_NUMBER, platform.architecture()[0], datetime.fromtimestamp(BUILD_TIME)))
         print "MIDE EBML library version %d.%d.%d" % ebml_version
@@ -341,6 +422,12 @@ if __name__ == "__main__":
         print "Source file(s) could not be found:"
         print "\n\t".join(missing)
         sys.exit(1)
+
+    if args.info is True:
+        print "=" * 70
+        for f in sourceFiles:
+            showInfo(f)
+        sys.exit(0)
         
     if args.output is not None:
         if not os.path.exists(args.output):
@@ -369,9 +456,9 @@ if __name__ == "__main__":
             updater.precision = max(0, min(2, (len(str(os.path.getsize(f)))/2)-1))
             updater(starting=True)
             totalSamples += raw2mat(f, matFilename=args.output, 
-                accelOnly=args.accelOnly, maxSize=args.maxSize,
-                startTime=args.startTime, endTime=endTime, 
-                updater=updater)
+                                    channels=args.channel, maxSize=args.maxSize,
+                                    startTime=args.startTime, endTime=endTime, 
+                                    updater=updater)
             updater(done=True)
     
         totalTime = datetime.now() - t0
@@ -379,6 +466,9 @@ if __name__ == "__main__":
         sampSec = locale.format("%d", totalSamples/totalTime.total_seconds(), grouping=True)
         totSamp = locale.format("%d", totalSamples, grouping=True)
         print "Conversion complete! Exported %s samples in %s (%s samples/sec.)" % (totSamp, tstr, sampSec)
+    except MATExportError as err:
+        print "*** Export error: %s" % err
+        exit(1)
     except KeyboardInterrupt:
         print "\n*** Conversion canceled! MAT version(s) of %s may be incomplete." % f
 #     except IOError as err: #Exception as err:
