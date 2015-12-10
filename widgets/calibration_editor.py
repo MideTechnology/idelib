1464111dcf09e19c4f887c7f6aa1aca23cd9ad7f
David Stokes <dstokes@mide.com>
2015-12-10 17:45:30 -0500

    Revised polynomial editor to work with either recorders or datasets.

--- widgets/calibration_editor.py
+++ widgets/calibration_editor.py
@@ -1,3 +1,5 @@
+"""
+"""
 
 import wx
 import wx.lib.sized_controls as SC
@@ -16,7 +18,7 @@ def cleanSplit(s, dtype=float):
 #===============================================================================
 
 class CoeffValidator(wx.PyValidator):
-    """ Validator for a text field containing coefficients.
+    """ Validator for a text field containing polynomial coefficients.
     """
     NAME = "coefficients"
     
@@ -28,6 +30,10 @@ class CoeffValidator(wx.PyValidator):
         self.root = root
         self.msg = ""
         self.polyType = polyType
+        self.polyName = ("%s polynomial" % self.polyType).strip().capitalize()
+        self.varName = self.NAME
+        self.num = 0
+        
         wx.PyValidator.__init__(self)
 
 
@@ -54,15 +60,16 @@ class CoeffValidator(wx.PyValidator):
             vals = None
             num = 0
 
-        ptype = ("%s polynomial" % self.polyType).strip().capitalize()
+        self.num = num
+        
         if vals is None:
-            err = "Invalid characters in %s!" % self.NAME
+            err = "Invalid characters in %s!" % self.varName
         elif self.min == self.max and self.min != num:
-            err = "%s must have exactly %d %s, %d given!" % (ptype, self.min, self.NAME, num)
+            err = "%(polyName)s must have exactly %(min)d %(varName)s, %(num)d given!" % self.__dict__
         elif self.max is None and num < self.min:
-            err = "%s must have at least %d %s, %d given!" % (ptype, self.min, self.NAME, num)
+            err = "%(polyName)s must have at least %(min)d %(varName)s, %(num)d given!" % self.__dict__
         elif num < self.min or num > self.max:
-            err = "%s must have %d to %d %s, %d given!" % (ptype, self.min, self.max, self.NAME, num)
+            err = "%(polyName)s must have %(min)d to %(max)d %(varName)s, %(num)d given!" % self.__dict__
         else:
             err = None
         
@@ -103,27 +110,29 @@ class CoeffValidator(wx.PyValidator):
         """
         return True # Prevent wxDialog from complaining.
 
+
 #===============================================================================
 
 class RefValidator(CoeffValidator):
+    """ Validator for a text field containing polynomial references.
+    """
     NAME = "reference value(s)"
-    
+
+
 #===============================================================================
 # 
 #===============================================================================
 
 class PolyEditDialog(SC.SizedDialog):
+    """ Dialog for editing Univariate and Bivariate polynomials.
     """
-    """
+    
     ID_UNIVARIATE = wx.NewId()
     ID_BIVARIATE = wx.NewId()
     
-    CAL_TYPE = Univariate
     DEFAULT_TITLE = "Edit Polynomial"
     DEFAULT_COEFFS = (1,0)
     DEFAULT_REFS = (1,)
-    NUM_COEFFS = (2,2)
-    NUM_REFS = (1,1)
     
     FIELD_PAD = 8 # Padding for use when calculating field height
     
@@ -132,11 +141,6 @@ class PolyEditDialog(SC.SizedDialog):
     # labels and warning icons.
     #===========================================================================
     
-    def _tostr(self, l):
-        if isinstance(l, (list, tuple)):
-            return '\n'.join(map(str, l))
-        return str(l)
-    
     def _addsubpane(self):
         # Helper helper. Don't use directly.
         subpane = SC.SizedPanel(self.pane, -1)
@@ -144,11 +148,15 @@ class PolyEditDialog(SC.SizedDialog):
         subpane.SetSizerProps(expand=True)
         return subpane
         
+        
     def _addfield(self, label, default=None, lines=1, **kwargs):
         """ Helper method to add a text field. """
-#         if isinstance(default, (list, tuple)) and lines == 1:
-#             lines = len(default)
-        default = self._tostr(default)
+        if isinstance(default, (list, tuple)):
+#             lines = lines if lines == 1 else len(default)
+            default = '\n'.join(map(str, default))
+        else:
+            default = str(default)
+            
         if lines > 1:
             kwargs['style'] = (kwargs.get('style', 0) 
                                | wx.TE_MULTILINE | wx.TE_PROCESS_ENTER)
@@ -160,11 +168,15 @@ class PolyEditDialog(SC.SizedDialog):
         icon = wx.StaticBitmap(subpane, -1, self.noBmp)
         icon.SetSizerProps(proportion=0, expand=False, valign="center")
         t.errIcon = icon
+        t.txt = txt
         return txt, t
+    
     
     def _addchoice(self, label, items, selected=None, enabled=True, **kwargs):
         """ Helper method to add a 'choice' widget (e.g. dropdown menu). """
-        wx.StaticText(self.pane, -1, label).SetSizerProps(valign="center")
+        # Very possibly overkill, since there's only one Choice field (for now)
+        txt = wx.StaticText(self.pane, -1, label)
+        txt.SetSizerProps(valign="center")
         subpane = self._addsubpane()
         c = wx.Choice(subpane, -1, choices=items, **kwargs)
         c.SetSizerProps(expand=True, proportion=1)
@@ -173,20 +185,49 @@ class PolyEditDialog(SC.SizedDialog):
         icon = wx.StaticBitmap(subpane, -1, self.noBmp)
         icon.SetSizerProps(proportion=0, expand=False, valign="center")
         c.errIcon = icon
+        c.txt = txt
         c.Enable(enabled)
         return c
+
+
+    def _enableField(self, field, enable=True):
+        """ Helper method to enable/disable all widgets associated with a field.
+        """
+        field.Enable(enable)
+        if hasattr(field, 'errIcon'):
+            field.errIcon.Enable(enable)
+        if hasattr(field, 'txt'):
+            field.txt.Enable(enable)
+
+
+    def _showField(self, field, show=True):
+        """ Helper method to show/hide all widgets associated with a field.
+        """
+        field.Show(show)
+        if hasattr(field, 'errIcon'):
+            field.errIcon.Show(show)
+        if hasattr(field, 'txt'):
+            field.txt.Show(show)
+
+
+    def _uses(self, t):
+        """ Helper method to determine if a (Sub)Channel uses the current cal.
+        """
+        if t.transform is None:
+            return False
+        return t.transform == self.cal.id or t.transform == self.cal
+
 
     #===========================================================================
     # 
     #===========================================================================
     
-    def __init__(self, parent, wxId, cal=None, channel=None, dataset=None, 
-                 polyType=None, changeType=True, **kwargs):
+    def __init__(self, parent, wxId, cal=None, channels=[], transforms={},
+                 polyType=None, changeType=True, changeSource=True, **kwargs):
         """
         """
-        self.dataset = dataset
         self.cal = cal
-        self.channel = channel
+        self.channels = channels
         self.polyType = polyType
         self.changeType = changeType
         self.prevType = None
@@ -199,12 +240,11 @@ class PolyEditDialog(SC.SizedDialog):
             self.cal = self.cal.copy()
             if self.polyType is None:
                 self.polyType = self.cal.__class__
-            if isinstance(self.cal, Bivariate) and self.dataset is not None:
+            if isinstance(self.cal, Bivariate) and self.channels:
                 try:
-                    self.calSubchannel = self.dataset.channels[self.cal.channelId][self.cal.subchannelId]
+                    self.calSubchannel = self.channels[self.cal.channelId][self.cal.subchannelId]
                 except (IndexError, KeyError):
                     pass
-                
         
         kwargs.setdefault('style', wx.DEFAULT_DIALOG_STYLE | wx.RESIZE_BORDER)
         super(PolyEditDialog, self).__init__(parent, wxId, **kwargs)
@@ -218,14 +258,16 @@ class PolyEditDialog(SC.SizedDialog):
         self.buildUI()
         
         if not self.changeType:
-            self.uniBtn.Enable(False)
-            self.biBtn.Enable(False)
+            self.typeTxt.Show(False)
+            self.uniBtn.Show(False)
+            self.biBtn.Show(False)
             
         if self.polyType == Bivariate:
             self.biBtn.SetValue(True)
-            self.sourceList.Enable(True)
+            self.sourceList.Enable(changeSource)
         else:
             self.uniBtn.SetValue(True)
+            self._showField(self.sourceList, False)
         
         self.SetButtonSizer(self.CreateStdDialogButtonSizer(wx.OK | wx.CANCEL))
         self.Fit()
@@ -256,8 +298,8 @@ class PolyEditDialog(SC.SizedDialog):
             ref = self.DEFAULT_REFS
         
         if self.cal is None:
-            if len(self.dataset.transforms) > 0:
-                self.calId = max(self.dataset.transforms) + 1
+            if len(self.transforms) > 0:
+                self.calId = max(self.transforms) + 1
             else:
                 self.calId = 0
             self.calLabel = '%d (New)' % self.calId
@@ -268,8 +310,8 @@ class PolyEditDialog(SC.SizedDialog):
         self.pane = self.GetContentsPane()
         self.pane.SetSizerType("form")
         
-        typeTxt = wx.StaticText(self.pane, -1, "Polynomial Type:")
-        self.lineHeight = typeTxt.GetTextExtent("yY")[1] 
+        self.typeTxt = wx.StaticText(self.pane, -1, "Polynomial Type:")
+        self.lineHeight = self.typeTxt.GetTextExtent("yY")[1] 
         subpane = self._addsubpane()
         self.uniBtn = wx.RadioButton(subpane, self.ID_UNIVARIATE, "Univariate")
         self.biBtn = wx.RadioButton(subpane, self.ID_BIVARIATE, "Bivariate")
@@ -302,38 +344,39 @@ class PolyEditDialog(SC.SizedDialog):
 
 
     def updateUI(self):
-        """
+        """ Handle updating the UI for the current type of polynomial being
+            edited.
         """
         if self.polyType == Univariate:
-            self.NUM_COEFFS = (2,2)
-            self.NUM_REFS = (1,1)
+            self.numCoeffs = (2,2)
+            self.numRefs = (1,1)
             self.sourceList.Enable(False)
         else:
-            self.NUM_COEFFS = (4,4)
-            self.NUM_REFS = (2,2)
+            self.numCoeffs = (4,4)
+            self.numRefs = (2,2)
             self.sourceList.Enable(True)
             if self.calSubchannel in self.sources:
                 self.sourceList.SetSelection(self.sources.index(self.calSubchannel))
             
         if self.polyType != self.prevType:
             ptype = self.polyType.__name__
-            coeffVal = CoeffValidator(*self.NUM_COEFFS, root=self, polyType=ptype)
-            refVal = RefValidator(*self.NUM_REFS, root=self, polyType=ptype)
+            coeffVal = CoeffValidator(*self.numCoeffs, root=self, polyType=ptype)
+            refVal = RefValidator(*self.numRefs, root=self, polyType=ptype)
             self.coeffField.SetValidator(coeffVal)
             self.refField.SetValidator(refVal)
             self.prevType = self.polyType
 
-        if self.NUM_COEFFS[0] == self.NUM_COEFFS[1]:
-            clabel = "Coefficients (%d)" % self.NUM_COEFFS[0]
+        if self.numCoeffs[0] == self.numCoeffs[1]:
+            clabel = "Coefficients (%d)" % self.numCoeffs[0]
         else:
-            clabel = "Coefficients (%d-%d)" % self.NUM_COEFFS
-        if self.NUM_REFS[0] == self.NUM_REFS[1]:
-            if self.NUM_REFS == 1:
+            clabel = "Coefficients (%d-%d)" % self.numCoeffs
+        if self.numRefs[0] == self.numRefs[1]:
+            if self.numRefs == 1:
                 rlabel = "Reference" 
             else: 
-                rlabel = "References (%d)" % self.NUM_REFS[0]
+                rlabel = "References (%d)" % self.numRefs[0]
         else:
-            rlabel = "References (%d-%d)" % self.NUM_REFS
+            rlabel = "References (%d-%d)" % self.numRefs
         self.coeffLbl.SetLabelText(clabel)
         self.refLbl.SetLabelText(rlabel)
 
@@ -376,41 +419,40 @@ class PolyEditDialog(SC.SizedDialog):
 
 
     def buildSourceList(self):
-        """ Get a list of all Channels using the current calibration polynomial.
+        """ Get a list of all Channels/SubChannels that can be used as the 
+            source for the polynomial being edited. Channels/SubChannels that
+            use the current polynomial are excluded to prevent circular 
+            references.
         """
         self.sources = []
         self.sourceNames = []
-        if self.dataset is not None:
-            for source in self.dataset.getPlots(plots=False, sort=False):
-                if self.channel is not None:
-                    if source == self.channel or source == self.channel.parent:
+        if not hasattr(self.cal, "channelId"):
+            # Probably shouldn't happen.
+            return
+        
+        if self.channels:
+            for c in self.channels.values():
+                if self._uses(c):
+                    # Exclude all subchannels of parent uses the current cal.
+                    continue
+                for source in c.subchannels:
+                    if self._uses(source):
                         continue
-                self.sources.append(source)
-                self.sourceNames.append("%d:%d %s" % (source.parent.id,
-                                                      source.id,
-                                                      source.displayName))
+                        
+                    self.sources.append(source)
+                    self.sourceNames.append("%d:%d %s" % (source.parent.id,
+                                                          source.id,
+                                                          source.displayName))
             
 
     def OnTypeChanged(self, evt):
-        """
+        """ Handle polynomial type change.
         """
         if evt.GetEventObject().GetId() == self.ID_UNIVARIATE:
             self.polyType = Univariate
         else:
             self.polyType = Bivariate
         self.updateUI()
-
-
-    #===========================================================================
-    # 
-    #===========================================================================
-    
-#     def editPolynomial(self, cal, dataset=None):
-
-#===============================================================================
-# 
-#===============================================================================
-
 
 
 #===============================================================================
@@ -420,19 +462,27 @@ class PolyEditDialog(SC.SizedDialog):
 if __name__ == '__main__':
     from mide_ebml import importer
     doc = importer.importFile()
-#     print doc.transforms
+    transforms = doc.transforms
+    channels = doc.channels
+    print "loaded %r" % doc.filename
     
     app = wx.App()
-    
+   
     def show(d, title="Edit Polynomial", cal=1, **kwargs):
-        d = d(None, -1, title=title, dataset=doc, cal=doc.transforms[cal], **kwargs)
+        title = "%s (ID %d)" % (title, cal)
+        d = d(None, -1, title=title, transforms=transforms, channels=channels, cal=transforms[cal], **kwargs)
         d.ShowModal()
         cal = d.cal
         d.Destroy()
+        if hasattr(cal, 'channelId'):
+            print "%r, refs channel %r.%r" % (cal, cal.channelId, cal.subchannelId)
+        else:
+            print "%r" % cal
         return cal
     
-    print "%r" % show(PolyEditDialog)
-    print "%r" % show(PolyEditDialog, changeType=False)
-    print "%r" % show(PolyEditDialog, cal=0, changeType=False)
+    show(PolyEditDialog, cal=1)
+    show(PolyEditDialog, cal=2, changeType=False)
+    show(PolyEditDialog, cal=3, changeType=False, changeSource=False)
+    show(PolyEditDialog, cal=0, changeType=False)
     
     app.MainLoop()
