b65f2a4ca28f6a4ec2d45ecabb27d1b42da9c126
David Stokes <dstokes@mide.com>
2016-07-12 12:12:37 -0400

    Working on ide2csv utility; added mean removal, info dump, UTC/ISO formatting, code cleanup.

---
+++ ide2mat/build_ide2csv.py
@@ -0,0 +1,112 @@
+import argparse
+from datetime import datetime
+import logging
+import os
+import subprocess
+import socket
+import sys
+import time
+
+
+HOME_DIR = os.getcwd()
+logger = logging.getLogger('SlamStickLab.BuildAll')
+
+builds = (
+    r'C:\Python27\Scripts\pyinstaller.exe --noconfirm --onefile --distpath="%(dist_32)s" -i .\ssx.ico ide2csv-win-onefile.spec',
+    r'c:\Python27_64\Scripts\pyinstaller --noconfirm --onefile --distpath="%(dist_64)s" --workpath=build_64 -i .\ssx.ico ide2csv-win-onefile.spec',
+)
+
+#===============================================================================
+# 
+#===============================================================================
+
+def writeInfo(version, debug, buildNum, buildTime, buildMachine):
+    with open('build_info_ide2csv.py', 'wb') as f:
+        f.write('# AUTOMATICALLY UPDATED FILE: EDIT WITH CAUTION!\n')
+        f.write('VERSION = %s\n' % str(version))
+        f.write('DEBUG = %s\n' % debug)
+        f.write('\n# AUTOMATICALLY-GENERATED CONTENT FOLLOWS; DO NOT EDIT MANUALLY!\n')
+        f.write('BUILD_NUMBER = %d\n' % buildNum)
+        f.write('BUILD_TIME = %d\n' % buildTime)
+        f.write('BUILD_MACHINE = %r\n' % buildMachine)
+
+#===============================================================================
+# 
+#===============================================================================
+
+parser = argparse.ArgumentParser(description="Multi-Target Builder")
+parser.add_argument('-v', '--version',  
+                    help="A new version number, as 'x.y.z'.")
+parser.add_argument('-r', '--release', action="store_true",
+                    help="Builds are for release; build without DEBUG.")
+parser.add_argument('-n', '--noincrement', action="store_true",
+                    help="Don't increase the build number.")
+args = parser.parse_args()
+print args
+
+#===============================================================================
+# 
+#===============================================================================
+
+t0 = datetime.now()
+
+try:
+    sys.path.append(HOME_DIR)
+    from build_info_ide2csv import BUILD_NUMBER, DEBUG, VERSION, BUILD_TIME, BUILD_MACHINE
+    
+    if args.version is not None:
+        thisVersion = map(int, filter(len, args.version.split('.')))
+        thisVersion = tuple(thisVersion + ([0] * (3-len(thisVersion)))) 
+    else:
+        thisVersion = VERSION
+    
+    thisBuildNumber = BUILD_NUMBER if args.noincrement else BUILD_NUMBER + 1
+    thisDebug = not args.release
+    thisTime = time.time()
+    
+    writeInfo(thisVersion, thisDebug, thisBuildNumber, thisTime, socket.gethostname())
+    versionString = '.'.join(map(str,thisVersion))
+
+except ImportError:
+    print "import error"
+    logger.warning("*** Couldn't read and/or change build number!")
+    thisBuildNumber = thisVersion = versionString = "Unknown"
+    thisDebug = True
+
+
+print "*"*78
+print ("*** Building Version %s, Build number %d," % (versionString,thisBuildNumber)),
+if thisDebug:
+    print "DEBUG version"
+else:
+    print "Release version"
+
+buildArgs = {
+    'dist_32': 'ide2csv v%s.%04d%s' % (versionString, thisBuildNumber, ' experimental' if thisDebug else ''),
+    'dist_64': 'ide2csv v%s.%04d%s' % (versionString, thisBuildNumber, ' experimental' if thisDebug else ''),
+}
+
+bad = 0
+for i, build in enumerate(builds):
+    print("="*78),("\nBuild #%d: %s\n" % (i+1, build % buildArgs)),("="*78)
+    bad += subprocess.call(build % buildArgs, stdout=sys.stdout, stdin=sys.stdin, shell=True)
+
+print "*"*78
+print "Completed %d builds, %d failures in %s" % (len(builds), bad, datetime.now() - t0)
+
+if bad == len(builds):
+    print "Everything failed; restoring old build_info."
+    writeInfo(VERSION, DEBUG, BUILD_NUMBER, BUILD_TIME, BUILD_MACHINE)
+else:
+    print "Version: %s, build %s, DEBUG=%s" % (versionString, thisBuildNumber, thisDebug)
+
+# if args.release and bad == 0:
+#     print "*"*78
+#     print "Everything is okay; updating version info file '%s'" % VERSION_INFO_FILE
+#     with open(VERSION_INFO_FILE,'w') as f:
+#         json.dump({"version": thisVersion, 
+#                    "changelog": CHANGELOG_URL, 
+#                    "date": int(thisTime)},
+#                   f)
+        
+print "*"*78
--- ide2mat/ide2csv.py
+++ ide2mat/ide2csv.py
@@ -125,8 +125,16 @@ class SimpleUpdater(object):
 #
 #===============================================================================
 
-def showInfo(ideFilename, toFile=False, **kwargs):
-    """
+def showInfo(ideFilename, toFile=False, extra=None, **kwargs):
+    """ Show information about an IDE file.
+    
+        @param ideFilename: The IDE file to show.
+        @keyword toFile: if `True`, the info will be written to a file with
+            the name of the IDE file plus ``_info.txt``.
+        @keyword extra: A dictionary of extra data to display (i.e. export
+            settings).
+        
+        Other keyword arguments are passed to `importer.openFile`.
     """
     if isinstance(toFile, basestring):
         base = os.path.splitext(os.path.basename(ideFilename))[0] + '_info.txt'
@@ -135,27 +143,69 @@ def showInfo(ideFilename, toFile=False, **kwargs):
     else:
         out = sys.stdout
     
+    idsort = lambda x: x.id
+    
     def _print(*s):
         out.write(' '.join(map(str, s)) + os.linesep)
         
-    _print(ideFilename)
-    out.write(("=" * 70) + os.linesep)
     with open(ideFilename, 'rb') as stream:
+        _print(ideFilename)
+        out.write(("=" * 70) + os.linesep)
         doc = importer.openFile(stream, **kwargs)
+        if len(doc.sessions) > 0:
+            st = doc.sessions[0].utcStartTime
+            if st:
+                _print('Start time: %s UTC' % (datetime.utcfromtimestamp(st)))
+        try:
+            info = doc.recorderInfo
+            partNum = info.get('PartNumber', '')
+            sn = info.get('RecorderSerial')
+            if partNum.startswith('LOG-0002'):
+                info['RecorderSerial'] = "SSX%07d" % sn
+            elif partNum.startswith('LOG-0003'):
+                info['RecorderSerial'] = "SSC%07d" % sn
+            _print('Recorder: %(ProductName)s, serial number %(RecorderSerial)s' % info)
+        except KeyError:
+            pass
+        _print()
+        
         _print("Sensors")
         _print( "-" * 40)
-        for s in sorted(doc.sensors.values()):
+        for s in sorted(doc.sensors.values(), key=idsort):
             _print( "  Sensor %d: %s" % (s.id, s.name))
             if s.traceData:
                 for i in s.traceData.items():
                     _print("    %s: %s" % i)
         _print()
+        
         _print("Channels")
         _print("-" * 40)
-        for c in sorted(doc.channels.values()):
+        for c in sorted(doc.channels.values(), key=idsort):
             _print("  Channel %d: %s" % (c.id, c.displayName))
             for sc in c.subchannels:
                 _print("    Subchannel %d.%d: %s" % (c.id, sc.id, sc.displayName))
+
+        if extra is not None:
+            _print()
+            _print("Export Options")
+            _print("-" * 40)
+            if extra.get('headers'):
+                _print("  * Column headers")
+            if extra.get('removeMean'):
+                if extra.get('meanSpan', 5.0) == -1:
+                    ms = 'Total mean removal'
+                else:
+                    ms = 'Rolling mean removal (%0.2f s)' % extra['meanSpan']
+                _print('  * %s on analog channels' % ms)
+            else:
+                _print('  * No mean removal from analog channels')
+                
+            if extra.get('useUtcTime'):
+                if extra.get('useIsoFormat'):
+                    _print('  * Timestamps in ISO format (yyyy-mm-ddThh:mm:ss.s')
+                else:
+                    _print("  * Timestamps in absolute UTC 'Unix' time")
+        
     _print("=" * 70)
     if out != sys.stdout:
         out.close()
@@ -167,11 +217,20 @@ def showInfo(ideFilename, toFile=False, **kwargs):
 
 def ideExport(ideFilename, outFilename=None, channels=None,
             startTime=0, endTime=None, updateInterval=1.5,  out=sys.stdout, 
-            outputType=".csv", delimiter=', ', headers=False, **kwargs):
+            outputType=".csv", delimiter=', ', headers=False, 
+            removeMean=True, meanSpan=5.0, useUtcTime=False,
+            useIsoFormat=False,
+            **kwargs):
     """ The main function that handles generating MAT files from an IDE file.
     """
     updater = kwargs.get('updater', importer.nullUpdater)
 #     maxSize = max(1024**2*16, min(matfile.MatStream.MAX_SIZE, 1024**2*maxSize))
+
+    if removeMean:
+        if meanSpan != -1:
+            meanSpan /= timeScalar
+    else:
+        meanSpan = None
 
     def _printStream(*args):
         out.write(" ".join(map(str, args)))
@@ -192,12 +251,14 @@ def ideExport(ideFilename, outFilename=None, channels=None,
     elif os.path.isdir(outFilename):
         outFilename = os.path.join(outFilename, os.path.splitext(b)[0])
 
+
     if outputType.lower().endswith('mat'):
         exporter = exportMat
         exportArgs = {}
     else:
         exporter = exportCsv
-        exportArgs = {'delimiter': delimiter}
+        exportArgs = {'delimiter': delimiter,
+                      'useIsoFormat': useIsoFormat}
         if outputType.lower().endswith('csv') and ',' not in delimiter:
             outputType = '.txt'
 
@@ -217,6 +278,8 @@ def ideExport(ideFilename, outFilename=None, channels=None,
             events = ch.getSession()
             numSamples += exporter(events, outName, callback=updater, 
                                    timeScalar=timeScalar, headers=headers,
+                                   removeMean=removeMean, meanSpan=meanSpan,
+                                   useUtcTime=useUtcTime,
                                    **exportArgs)[0]
 
         except None:
@@ -234,14 +297,19 @@ if __name__ == "__main__":
     import argparse
     from glob import glob
 
-    argparser = argparse.ArgumentParser(description="Mide Batch .IDE Converter v%d.%d.%d - Copyright (c) 2016 Mide Technology")
+    argparser = argparse.ArgumentParser(description="Mide Batch .IDE Converter v%d.%d.%d - Copyright (c) 2016 Mide Technology" % VERSION)
     argparser.add_argument('-o', '--output', help="The output path to which to save the exported files. Defaults to the same as the source file.")
-    argparser.add_argument('-n', '--names', action='store_true', help="Write channel names as the first row of text-based export (not applicable to MAT export).")
-    argparser.add_argument('-d', '--delimiter', choices=('comma','tab','pipe'), help="The delimiting character (not applicable to MAT export).", default="comma")
     argparser.add_argument('-t', '--type', help="The type of file to export.", choices=('csv','mat','txt'), default="csv")
     argparser.add_argument('-c', '--channel', action='append', type=int, help="Export the specific channel. Can be used multiple times. If not used, all channels will export.")
-    argparser.add_argument('-m', '--nomean', action='store_true', help="Do not remove mean from analog channels.")
-    argparser.add_argument('-s', '--span', type=float, default=5.0, help="Length of rolling mean span (seconds) when removing the mean from analog channels. -1 will remove the total mean.")
+#     argparser.add_argument('-m', '--meanspan', action='store_true', help="Do not remove mean (DC offset) from analog channels.")
+    argparser.add_argument('-m', '--meanspan', type=float, default=5.0, help="Length (in seconds) of rolling mean span (DC offset) when removing the mean from analog channels. -1 will remove the total mean. 0 will disable mean removal. Defaults to 5 seconds.")
+    argparser.add_argument('-u', '--utc', action='store_true', help="Write timestamps as UTC 'Unix' time.")
+    
+    txtargs = argparser.add_argument_group("Text Export Options (CSV, TXT, etc.)")
+    txtargs.add_argument('-n', '--names', action='store_true', help="Write channel names as the first row of text-based export.")
+    txtargs.add_argument('-d', '--delimiter', choices=('comma','tab','pipe'), help="The delimiting character.", default="comma")
+    txtargs.add_argument('-f', '--isoformat', action='store_true', help="Write timestamps as ISO-formatted UTC.")
+    
     argparser.add_argument('-i', '--info', action='store_true', help="Show information about the file and exit.")
     argparser.add_argument('-v', '--version', action='store_true', help="Show detailed version information and exit.")
     argparser.add_argument('source', nargs="*", help="The source .IDE file(s) to convert.")
@@ -285,32 +353,39 @@ if __name__ == "__main__":
 
     delimiter = {'tab': '\t', 'pipe': ' | '}.get(args.delimiter, ', ')
 
-    removeMean = not args.nomean
-    meanSpan = args.span
-    if meanSpan == 0:
-        removeMean = False
-    elif meanSpan != -1:
-        meanSpan /= timeScalar
-
+    meanSpan = args.meanspan
+    removeMean = meanSpan != 0
+    
+    useUtcTime = args.utc
+    useIsoFormat = args.isoformat
+    if 'mat' not in args.type.lower():
+        useUtcTime = useUtcTime or useIsoFormat
+    else:
+        useIsoFormat = False
+    
     try:
         totalSamples = 0
         t0 = datetime.now()
         updater=SimpleUpdater()
         for f in sourceFiles:
             print ('Converting "%s"...' % f)
-            if 'mat' not in args.type.lower():
-                showInfo(f, toFile=args.output)
+            
+            exportArgs = dict(outFilename=args.output,
+                              channels=args.channel,
+                              outputType=args.type,
+                              delimiter=delimiter,
+                              updater=updater,
+                              headers=args.names,
+                              removeMean=removeMean,
+                              meanSpan=meanSpan,
+                              useUtcTime=useUtcTime,
+                              useIsoFormat=useIsoFormat)
+            
+            showInfo(f, toFile=args.output, extra=exportArgs)
+                
             updater.precision = max(0, min(2, (len(str(os.path.getsize(f)))/2)-1))
             updater(starting=True)
-            totalSamples += ideExport(f,
-                                      outFilename=args.output,
-                                      channels=args.channel,
-                                      outputType=args.type,
-                                      delimiter=delimiter,
-                                      updater=updater,
-                                      headers=args.names,
-                                      removeMean=removeMean,
-                                      meanSpan=meanSpan)
+            totalSamples += ideExport(f, **exportArgs)
 
         totalTime = datetime.now() - t0
         tstr = str(totalTime).rstrip('0.')
