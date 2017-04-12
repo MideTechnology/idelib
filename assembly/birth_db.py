f837dea757d473e75a1abe2d63664526be317118
David Stokes <dstokes@mide.com>
2017-04-12 10:48:43 -0400

    Minor work on birth DB API.

--- assembly/birth_db.py
+++ assembly/birth_db.py
@@ -6,6 +6,15 @@ An interface for the SlamStick birthing and calibration database.
 The first version establishes the API but still uses the old system (a 
 collection of files on the shared drive.
 
+High-level representations will be used where possible; recorders will be
+represented as instances of `Recorder` subclasses, polynomials as polynomial
+objects, etc.
+
+@todo: Possibly add methods and/or properties to `Recorder` subclasses like 
+    `setCalibration()` for various read-only attributes. These will raise 
+    exceptions if the device is 'real' and/or used outside of the birthing
+    process. Maybe a specialized mix-in class, as not to pollute the basic
+    representation. 
 '''
 
 __author__ = "dstokes"
@@ -30,6 +39,8 @@ TEMPLATE_PATH = os.path.join(BIRTHER_PATH, "data_templates")
 DB_PATH = os.path.join(PRODUCT_ROOT_PATH, "Product_Database")
 CAL_PATH = os.path.join(DB_PATH, '_Calibration')
 
+# The current files. The first iteration of the 'database' will still use
+# these, and the real database (using the same API) will be swapped in later.
 DEV_SN_FILE = os.path.join(DB_PATH, 'last_sn.txt')
 CAL_SN_FILE = os.path.join(DB_PATH, 'last_cal_sn.txt')
 
@@ -89,8 +100,8 @@ class Database(object):
         """
     
 
-    def newRecorder(self, chipId, parNum, hwRev=None, fwRev=None, 
-                    serialNum=None, custom=False, size=8):
+    def newRecorder(self, chipId, partNum, hwRev=None, fwRev=None, 
+                    serialNum=None, custom=False, size=8, batch=None):
         """ Add a new recorder to the database. 
         
             @param chipId: The recorder's unique MCU ID.
@@ -104,36 +115,74 @@ class Database(object):
                 already exists.
             @keyword custom: `True` if the recorder is a custom job.
             @keyword size: The size of the recorder's flash storage.
-            
+            @keyword batch: The ID of the recorder's batch (i.e. a shipment
+                from the factory).
+            @return: An instance of a `Recorder` subclass. 
         """
 
+    
+    def saveRecorder(self, recorder):
+        """ Update a recorder's entry in the database.
+        
+            @param recorder: The recorder to save, an instance of a `Recorder`
+                subclass.
+        """
+        
+
+    def newBatch(self, batchId, date, manufacturer):
+        """ Create a new batch record.
+            
+            @param batchId: The batch's ID code.
+            @param date: Batch manufacturing date.
+            @param manufacturer: The manufacturer of the batch.
+        """
+    
+    
+    def getBatch(self, batchId):
+        """ Get all recorders from a batch.
+            
+            @param batchId: The batch's ID code.
+        """
+    
 
     #===========================================================================
     # 
     #===========================================================================
 
-    def getCalibration(self, serialNum):
+    def getCalibration(self, calId):
+        """ Get the database's latest calibration information for a given 
+            calibration ID.
+        
+            @param calId: The calibration ID (serial number).
+            @return: A list of calibration polynomials.
+        """
+    
+        
+    def getRecorderCalibration(self, serialNum):
         """ Get the database's latest calibration information for a given 
             recorder.
         
             @param serialNum: The recorder's serial number.
+            @return: A list of calibration polynomials.
         """
     
         
     def addCalibration(self, serialNum, polynomial, calId=None, new=False, 
-                       date=None, humidity=None, referenceId=None):
+                       date=None, humidity=None, referenceId=None, user=None):
         """ Save a new calibration polynomial to the database.
         
             @param serialNum: The recorder's serial number.
-            @param polynomail: The `UnivariatePolynomial` or 
+            @param polynomial: The `UnivariatePolynomial` or 
                 `BivariatePolynomial` to add.
-            @keyword calId: The calibration ID. Defaults to the latest (since
-                one calibration session contains multiple channels), unless
-                `new` is `True`.
+            @keyword calId: The calibration ID (serial number). Defaults to the
+                latest (since one calibration session contains multiple 
+                channels), unless `new` is `True`.
             @keyword new: If `True`, a new calibration ID is generated.
             @keyword date: The calibration date/time. Defaults to present.
             @keyword humidity: 
             @keyword referenceId: 
+            @keyword user: The name/ID of the technician who performed the
+                calibration. Defaults to the current user.
             @return: The calibration ID.
         """
     
@@ -151,5 +200,18 @@ class Database(object):
             @keyword calId: The calibration ID. Defaults to the latest.
         """
         
+    #===========================================================================
+    # 
+    #===========================================================================
 
+    
+    def findRecorder(self, *args, **kwargs):
+        """ Search for recorders. This will probably not get implemented until
+            the real database. Actual arguments TBD.
+        """
+
+    def findBatch(self, *args, **kwargs):
+        """ Search for batches. This will probably not get implemented until
+            the real database. Actual arguments TBD.
+        """
 
