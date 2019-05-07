9377a25e0f486afc74119389ddbb44e672e63bd4
David Stokes <dstokes@mide.com>
2019-05-07 11:02:42 -0400

    Update editor.py

--- scripting/editor.py
+++ scripting/editor.py
@@ -1,6 +1,8 @@
 import  wx
 import  wx.stc  as  stc
 
+from base import MenuMixin
+
 from python_stc import PythonSTC, faces
 
 # XXX: hack, get rid of bad demo fonts!
@@ -24,3 +26,72 @@ class ScriptEditorCtrl(PythonSTC):
             self.CallTipCancel()
             
         event.Skip()
+
+
+#===============================================================================
+# 
+#===============================================================================
+
+class ScriptEditor(wx.Frame, MenuMixin):
+    """
+    """
+    
+    def __init__(self, *args, **kwargs):
+        """
+        """
+        super(ScriptEditor, self).__init__(*args, **kwargs)
+        
+        sizer = wx.BoxSizer()
+        self.editor = ScriptEditorCtrl(self, -1)
+        sizer.Add(self.editor, 1, wx.EXPAND)
+
+
+    def buildMenus(self):
+        """
+        """
+        menu = wx.MenuBar()
+
+        # "File" menu
+        #=======================================================================
+        fileMenu = self.addMenu(self.menubar,  '&File')
+        self.addMenuItem(fileMenu, wx.ID_NEW, "&New Script Editor\tCtrl+N", "",
+                         self.OnFileNewMenu)
+        self.addMenuItem(fileMenu, wx.ID_CLOSE, 
+                         "Close Script Editor\tCtrl+W", "", self.OnClose)
+        fileMenu.AppendSeparator()
+        self.addMenuItem(fileMenu, wx.ID_OPEN, u"", u"", 
+                         self.OnFileOpenMenu)
+        fileMenu.AppendSeparator()
+        
+        fileMenu.AppendSeparator()
+        self.addMenuItem(fileMenu, wx.ID_PRINT, u"", u"", enabled=False)
+        self.addMenuItem(fileMenu, wx.ID_PRINT_SETUP, u"", u"", enabled=False)
+        
+#         fileMenu.AppendSeparator()
+#         self.recentFilesMenu = self.app.recentFilesMenu #wx.Menu()
+#         fileMenu.AppendMenu(self.ID_RECENTFILES, "Recent Files", 
+#                             self.recentFilesMenu)
+#         fileMenu.AppendSeparator()
+
+        
+        # "Edit" menu
+        #=======================================================================
+        editMenu = self.addMenu(self.menubar, '&Edit')
+        editMenu.Append(wx.ID_CUT)
+        editMenu.Append(wx.ID_COPY)
+        editMenu.Append(wx.ID_PASTE)
+#         editMenu.AppendSeparator()
+#         self.addMenuItem(editMenu, wx.ID_PREFERENCES, "Preferences...", "",
+#                          self.app.editPrefs)
+
+
+
+#===============================================================================
+# 
+#===============================================================================
+
+if __name__ == "__main__":
+    app = wx.App()
+    dlg = ScriptEditor(None)
+    dlg.Show()
+    app.MainLoop()
