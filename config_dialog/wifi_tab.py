2d727119a91e64255a2341365fd24717106f1558
David Stokes <dstokes@mide.com>
2019-11-14 16:18:53 -0500

    More Wi-Fi config dialog work

--- config_dialog/wifi_tab.py
+++ config_dialog/wifi_tab.py
@@ -57,10 +57,16 @@ class WifiSelectionTab(SC.SizedPanel):#Tab):
         self.info = [WifiInfo("MIDE-Corp", .9, 'WPA', True),
                      WifiInfo("MIDE-Guest", .7, 'WPA2', False),
                      WifiInfo("EarthAP", .5, None, True),
-                     WifiInfo("MoonAP", .3, 'WPA', False)]
+                     WifiInfo("MoonAP", .3, 'WPA', False),
+                     WifiInfo("PlutoAP", -1, None, True)]
         
         self.lastSSID = "Missing" #self.info[2][0]
     
+        if self.lastSSID not in (i.ssid for i in self.info):
+            self.info.append(WifiInfo(self.lastSSID, -1, None, True))
+
+#         self.info = [x for x in self.info if x.ssid not in self.deleted]
+            
     
     def initUI(self):
         """ Build the user interface, populating the Tab. 
@@ -97,41 +103,58 @@ class WifiSelectionTab(SC.SizedPanel):#Tab):
         font = self.list.GetFont()
         self.boldFont = font.Bold()
         self.italicFont = font.Italic()
+        self.struckFont = font.Strikethrough()
         self.notFoundColor = wx.Colour(127,127,127)
         
         # Rescan button (and footnote text)
         scansizer = wx.BoxSizer(wx.HORIZONTAL)
         rescanLabel = wx.StaticText(self, -1, "* Currently configured AP")
         rescan = wx.Button(self, -1, "Rescan")
-        rescanLabel.SetFont(self.italicFont)
-        scansizer.Add(rescanLabel, 1, wx.EXPAND|wx.ALIGN_LEFT|wx.ALL, 8)
-        scansizer.Add(rescan, 0, wx.ALIGN_RIGHT|wx.EAST|wx.SHAPED, 8)
+        rescanLabel.Enable(False)
+        scansizer.AddMany(((rescanLabel, 1, wx.EXPAND|wx.ALIGN_LEFT|wx.ALL, 8),
+                           (rescan, 0, wx.ALIGN_RIGHT|wx.EAST|wx.SHAPED, 8)))
         sizer.Add(scansizer, 0, wx.EXPAND)
 
         # Password field components
         pwsizer = wx.BoxSizer(wx.HORIZONTAL)
-        labelsize = self.GetTextExtent("Change Password:")
-        self.pwCheck = wx.CheckBox(self, -1, "")
-        self.pwLabel = wx.StaticText(self, -1, "Change Password:",
-                                     size=labelsize, style=wx.ALIGN_RIGHT)
+        labelsize = self.GetTextExtent("Change Password:") + wx.Size(20,0)
+        self.pwCheck = wx.CheckBox(self, -1, "Change Password:",
+                                     size=labelsize)
         self.pwField = wx.TextCtrl(self, -1, style=wx.TE_PASSWORD)
         self.pwCheck.Enable(False)
-        self.pwLabel.Enable(False)
         self.pwField.Enable(False)
         
         pwstyle = wx.ALIGN_CENTER_VERTICAL|wx.RESERVE_SPACE_EVEN_IF_HIDDEN
         pwsizer.AddMany(((self.pwCheck, 0, pwstyle),
-                         (self.pwLabel, 0, pwstyle|wx.ALIGN_RIGHT|wx.EAST, 4),
                          (self.pwField, 1, pwstyle|wx.EXPAND)))
         sizer.Add(pwsizer, 0, wx.EXPAND|wx.ALL, 8)
 
+        self.forgetCheck = wx.CheckBox(self, -1, "Forget this AP on exit")
+        sizer.Add(self.forgetCheck, 0, wx.EXPAND|wx.WEST|wx.SOUTH, 8)
+
         self.SetSizer(sizer)
 
+        # For doing per-item tool tips in the list
+        self.listToolTips = []
+        self.lastToolTipItem = -1
+
+        self.deleted = []
+        self.passwords = {}
+        
+        # for wxMSW
+        self.list.Bind(wx.EVT_COMMAND_RIGHT_CLICK, self.OnListRightClick)
+        # for wxGTK
+        self.list.Bind(wx.EVT_RIGHT_UP, self.OnListRightClick)
+
+        self.list.Bind(wx.EVT_MOTION, self.OnListMouseMotion)
         rescan.Bind(wx.EVT_BUTTON, self.OnRescan)
         self.Bind(wx.EVT_LIST_ITEM_SELECTED, self.OnItemSelected, self.list)
         self.Bind(wx.EVT_LIST_ITEM_DESELECTED, self.OnItemDeselected, self.list)
         self.pwField.Bind(wx.EVT_SET_FOCUS, self.OnPasswordFocus)
+        self.pwField.Bind(wx.EVT_TEXT, self.OnPasswordText)
+        self.forgetCheck.Bind(wx.EVT_CHECKBOX, self.OnForgetChecked)
 
+        self.getInfo()
         self.populate()
         
 
@@ -142,38 +165,46 @@ class WifiSelectionTab(SC.SizedPanel):#Tab):
         self.lastSelected = -2
         
         self.list.DeleteAllItems()
-        self.getInfo()
         
+        self.listToolTips = []
+
+        self.forgetCheck.Enable(False)
+
         knownAPs = []
-        for ap in self.info:
-            icon = self.icons[int(ap.strength * 5)]
+        for n, ap in enumerate(self.info):
+            tooltip = "%s" % ap.ssid
+            icon = max(0, self.icons[int(ap.strength * 5)])
             idx = self.list.InsertItem(self.list.GetItemCount(), ap.ssid, icon)
             self.list.SetItem(idx, 1, str(ap.security))
+            self.list.SetItemData(idx, n)
             
-            if not ap.known:
-                # AP not previously configured; mark it.
-                item = self.list.GetItem(idx)
-                item.SetFont(self.italicFont)
-                self.list.SetItem(item)
+            item = self.list.GetItem(idx)
             if ap.ssid == self.lastSSID:
                 # The previously selected AP
-                item = self.list.GetItem(idx)
                 item.SetFont(self.boldFont)
                 item.SetText(item.GetText() + " *")
-                self.list.SetItem(item)
+                tooltip += " (saved, currently selected)"
+            elif not ap.known:
+                # AP not previously configured; mark it.
+                item.SetFont(self.italicFont)
+                tooltip += " (not saved)"
+            elif ap.ssid in self.deleted:
+                item.SetFont(self.struckFont)
+                tooltip += " (saved, will be forgotten)"
+            else:
+                tooltip += " (saved)"
             
+            
+            if ap.strength < 0:
+                item.SetTextColour(self.notFoundColor)
+                item.SetImage(self.icons[-1])
+                tooltip += "\nNot in range"
+                
+            self.list.SetItem(item)
             knownAPs.append(ap.ssid)
         
-        # Previously selected AP isn't in the list (out of range?)
-        if self.lastSSID not in knownAPs:
-            ssid = self.lastSSID + " *"
-            idx = self.list.InsertItem(self.list.GetItemCount(), ssid,
-                                       self.icons[-1])
-            item = self.list.GetItem(idx)
-            item.SetTextColour(self.notFoundColor)
-            item.SetFont(self.boldFont)
-            self.list.SetItem(item)
-        
+            self.listToolTips.append(tooltip)
+
 
     #===========================================================================
     # 
@@ -182,32 +213,45 @@ class WifiSelectionTab(SC.SizedPanel):#Tab):
     def OnRescan(self, evt):
         """ Handle "Rescan" button press.
         """
+        self.getInfo()
         self.populate()
 
 
     def OnItemSelected(self, evt): 
         """ Handle an AP list item getting selected.
         """
-        self.pwLabel.Enable()
+        self.pwCheck.Enable()
         self.pwField.Enable()
         
-        self.selected = evt.GetIndex()
-        if self.info[self.selected].known:
-            self.pwCheck.SetValue(False)
-            self.pwCheck.Enable(True)
-            self.pwLabel.SetLabelText("Change Password:")
+        self.selected = evt.GetItem().GetData()
+        ap = self.info[self.selected]
+        
+        changedPw = ap.ssid in self.passwords
+        
+        if ap.known:
+            self.pwCheck.SetLabelText("Change Password:")
         else:
-            self.pwCheck.Enable(False)
-            self.pwCheck.SetValue(True)
-            self.pwLabel.SetLabelText("Set Password:")
+            self.pwCheck.SetLabelText("Set Password:")
 
-        self.pwField.SetValue("")
+        self.pwCheck.SetValue(changedPw)
+        self.forgetCheck.Enable(ap.known)
+        
+        self.forgetCheck.SetValue(ap.ssid in self.deleted)
+        
+        self.pwField.SetValue(self.passwords.get(ap.ssid, ""))
 
 
     def OnItemDeselected(self, evt):
         """ Handle an AP list item getting deselected.
         """
-        self.selected = 0
+        self.selected = -1
+
+
+    def OnForgetChecked(self, evt):
+        """
+        """
+        self.deleted.append(self.info[self.selected].ssid)
+        self.populate()
 
 
     def OnPasswordFocus(self, evt):
@@ -216,11 +260,62 @@ class WifiSelectionTab(SC.SizedPanel):#Tab):
         # Erase the field unless the user hasn't changed selected AP
         if self.selected != self.lastSelected:
             self.pwField.SetValue('')
-            self.pwCheck.SetValue(True)
             self.lastSelected = self.selected
             
         evt.Skip()
 
+    
+    def OnPasswordText(self, evt):
+        """
+        """
+        text = evt.GetString()
+        if text:
+            self.pwCheck.SetValue(True)
+            self.passwords[self.info[self.selected].ssid] = evt.GetString()
+        evt.Skip()
+
+
+    def OnListMouseMotion(self, evt):
+        """ Handle mouse movement, updating the tool tips, etc.
+        """
+        # This determines the list item under the mouse and shows the
+        # appropriate tool tip, if any
+        index, _ = self.list.HitTest(evt.GetPosition())
+        if index != -1 and index != self.lastToolTipItem:
+            try:
+                item = self.list.GetItemData(index)
+                if self.listToolTips[item] is not None:
+                    self.list.SetToolTip(self.listToolTips[item])
+                else:
+                    self.list.UnsetToolTip()
+            except IndexError:
+                pass
+            self.lastToolTipItem = index
+        evt.Skip()
+        
+
+    def OnListRightClick(self, evt):
+        """ Handle a list item being right-clicked.
+        """
+        selected = self.info[self.selected]
+        
+        menu = wx.Menu()
+        mi = menu.Append(wx.ID_DELETE, 'Forget "%s"' % selected.ssid)
+        self.Bind(wx.EVT_MENU, self.OnDelete, id=wx.ID_DELETE)
+        
+        if not (selected.known and selected.strength < 0): 
+            mi.Enable(False)
+
+        self.PopupMenu(menu)
+        menu.Destroy()
+
+
+    def OnDelete(self, evt):
+        """ Delete (forget) a saved AP.
+        """
+        self.deleted.append(self.info[self.selected].ssid)
+        self.populate()
+        
 
     #===========================================================================
     # 
@@ -229,8 +324,8 @@ class WifiSelectionTab(SC.SizedPanel):#Tab):
     def save(self):
         """ Save Wi-Fi configuration data to the device.
         """
-        pass
-
+        # Any password changes should be written here.
+        # Any SSIDs in self.deleted should be deleted here.
 
 
 #===============================================================================
