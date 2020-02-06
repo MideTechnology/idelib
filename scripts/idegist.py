38ef3698a8442c59edd00ee2e89579e0b2e46116
Becker Awqatty <bawqatty@mide.com>
2020-02-06 11:04:37 -0500

    refactored script as callable function

--- scripts/idegist.py
+++ scripts/idegist.py
@@ -27,48 +27,52 @@ def ide_files_in(dirpath):
     )
 
 
-for filename in ide_files_in(".\\ide_files\\"):  # use your own pathname here
-    ds = idelib.importer.importFile(filename)
+def bulk_summarize(dirpath):
+    for filename in ide_files_in(".\\ide_files\\"):  # use your own pathname here
+        ds = idelib.importer.importFile(filename)
 
-    for channel in ds.channels.values():
-        for subchannel in channel.subchannels:
-            eventlist = subchannel.getSession()
-            session = eventlist.session
-            array = eventlist[:]
+        for channel in ds.channels.values():
+            for subchannel in channel.subchannels:
+                eventlist = subchannel.getSession()
+                session = eventlist.session
+                array = eventlist[:]
 
-            times = array[0]
-            values = array[1]
-            utc_times = np.datetime64(session.utcStartTime, 's') + times.astype('timedelta64[us]')
+                times = array[0]
+                values = array[1]
+                utc_times = np.datetime64(session.utcStartTime, 's') + times.astype('timedelta64[us]')
 
-            ts = np.diff(times*1e-6).mean()
-            fs = 1/ts
+                ts = np.diff(times*1e-6).mean()
+                fs = 1/ts
 
-            norm_values = values - values.mean()
+                norm_values = values - values.mean()
 
-            freqs = np.fft.rfftfreq(len(values), d=ts)
-            psd_values = np.abs(np.fft.rfft(norm_values, norm='ortho'))**2
-            f_argmax = np.argmax(psd_values)
+                freqs = np.fft.rfftfreq(len(values), d=ts)
+                psd_values = np.abs(np.fft.rfft(norm_values, norm='ortho'))**2
+                f_argmax = np.argmax(psd_values)
 
-            print(
-                ds.filename,
-                ds.recorderInfo['RecorderSerial'],
-                ds.recorderInfo['PartNumber'],
-                subchannel.name,
-                np.datetime64(session.utcStartTime, 's'),
-                np.datetime64(session.utcStartTime, 's') + np.array(session.lastTime-session.firstTime, dtype='timedelta64[us]'),
-                # low Hz
-                values.min(),
-                values.mean(),
-                values.max(),
-                # high Hz
-                norm_values.min(),
-                norm_values.max(),
-                values.std(),  # same as RMS
-                freqs[f_argmax],
-                psd_values[f_argmax],
-            )
-            print()
+                print(
+                    ds.filename,
+                    ds.recorderInfo['RecorderSerial'],
+                    ds.recorderInfo['PartNumber'],
+                    subchannel.name,
+                    np.datetime64(session.utcStartTime, 's'),
+                    np.datetime64(session.utcStartTime, 's') + np.array(session.lastTime-session.firstTime, dtype='timedelta64[us]'),
+                    # low Hz
+                    values.min(),
+                    values.mean(),
+                    values.max(),
+                    # high Hz
+                    norm_values.min(),
+                    norm_values.max(),
+                    values.std(),  # same as RMS
+                    freqs[f_argmax],
+                    psd_values[f_argmax],
+                )
+                print()
 
 
-    ds.close()  # Remember to close your file after you're finished with it!
-    del ds, channel, subchannel, eventlist, session  # These all depend on the internal file object
+        ds.close()  # Remember to close your file after you're finished with it!
+        del ds, channel, subchannel, eventlist, session  # These all depend on the internal file object
+
+
+bulk_summarize(".\\ide_files\\")
