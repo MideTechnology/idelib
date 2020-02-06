b2402c03b0598bd9e84ca79ac3b9c1b4336d7383
Becker Awqatty <bawqatty@mide.com>
2020-02-06 17:28:49 -0500

    changed to loop over all subchannels
    - changed to operate on 1D value arrays
    - refactored `(s)ch` -> `(sub)channel`

--- scripts/idegist.py
+++ scripts/idegist.py
@@ -18,44 +18,45 @@ filename = 'test.IDE'  # use your own filename here
 
 ds = idelib.importer.importFile(filename)
 
-for ch in ds.channels.values():
-    eventlist = ch.getSession()
-    session = eventlist.session
-    array = eventlist[:]
+for channel in ds.channels.values():
+    for subchannel in channel.subchannels:
+        eventlist = subchannel.getSession()
+        session = eventlist.session
+        array = eventlist[:]
 
-    times = array[0]
-    values = array[1:]
-    utc_times = np.datetime64(session.utcStartTime, 's') + times.astype('timedelta64[us]')
+        times = array[0]
+        values = array[1]
+        utc_times = np.datetime64(session.utcStartTime, 's') + times.astype('timedelta64[us]')
 
-    ts = np.diff(times*1e-6).mean()
-    fs = 1/ts
+        ts = np.diff(times*1e-6).mean()
+        fs = 1/ts
 
-    norm_values = values - values.mean(axis=1).reshape(-1, 1)
+        norm_values = values - values.mean()
 
-    freqs = np.fft.rfftfreq(values.shape[1], d=ts)
-    psd_values = np.abs(np.fft.rfft(norm_values, norm='ortho', axis=1))**2
-    f_argmax = np.argmax(psd_values, axis=1)
+        freqs = np.fft.rfftfreq(len(values), d=ts)
+        psd_values = np.abs(np.fft.rfft(norm_values, norm='ortho'))**2
+        f_argmax = np.argmax(psd_values)
 
-    print(
-        ds.filename,
-        ds.recorderInfo['RecorderSerial'],
-        ds.recorderInfo['PartNumber'],
-        ch.name,
-        np.datetime64(session.utcStartTime, 's'),
-        np.datetime64(session.utcStartTime, 's') + np.array(session.lastTime-session.firstTime, dtype='timedelta64[us]'),
-        # low Hz
-        values.min(axis=1),
-        values.mean(axis=1),
-        values.max(axis=1),
-        # high Hz
-        norm_values.min(axis=1),
-        norm_values.max(axis=1),
-        values.std(axis=1),  # same as RMS
-        freqs[f_argmax],
-        psd_values[(np.arange(len(f_argmax)), f_argmax)],
-    )
-    print()
+        print(
+            ds.filename,
+            ds.recorderInfo['RecorderSerial'],
+            ds.recorderInfo['PartNumber'],
+            subchannel.name,
+            np.datetime64(session.utcStartTime, 's'),
+            np.datetime64(session.utcStartTime, 's') + np.array(session.lastTime-session.firstTime, dtype='timedelta64[us]'),
+            # low Hz
+            values.min(),
+            values.mean(),
+            values.max(),
+            # high Hz
+            norm_values.min(),
+            norm_values.max(),
+            values.std(),  # same as RMS
+            freqs[f_argmax],
+            psd_values[f_argmax],
+        )
+        print()
 
 
 ds.close()  # Remember to close your file after you're finished with it!
-del ds, ch, eventlist, session  # These all depend on the internal file object
+del ds, channel, subchannel, eventlist, session  # These all depend on the internal file object
