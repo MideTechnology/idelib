beb8187b2e24fae4e98d6d783d01e5096ce2db74
Becker Awqatty <bawqatty@mide.com>
2020-02-21 12:46:47 -0500

    added tool metadata to multi_file_summarize.py

--- tools/idegist.py
+++ tools/idegist.py
@@ -1,1 +1,19 @@
 import scripts.idegist
+
+
+###############################################################################
+# Tool Metadata
+###############################################################################
+
+__author__ = "B. B. Awqatty"
+__email__ = "bawqatty@mide.com"
+__version_tuple__ = (0, 0, 0)
+__version__= ".".join([str(num) for num in __version_tuple__])
+__copyright__= "Copyright (c) 2020 Mid\xe9 Technology"
+
+
+PLUGIN_INFO = {
+    "type": "tool",
+    "name": "Batch IDE Summarizer",
+    "app": u"enDAQ Lab",
+}
