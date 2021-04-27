16f1260ea006b95f7346893c165480f2ce4b245f
MIDE\cflanigan <cflanigan@mide.com>
2021-04-27 16:16:56 -0400

    No changes requested, finalizing release.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ EXAMPLE_REQUIRES = [
 
 setuptools.setup(
         name='idelib',
-        version='3.1.0a1',
+        version='3.1.0',
         author='Mide Technology',
         author_email='help@mide.com',
         description='Python API for accessing IDE data recordings',
