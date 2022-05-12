1ac602a112b4daca633852d900972dc49d449900
David Stokes <dstokes@mide.com>
2022-05-12 18:13:51 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ EXAMPLE_REQUIRES = [
 
 setuptools.setup(
         name='idelib',
-        version='3.2.4',
+        version='3.2.5',
         author='Mide Technology',
         author_email='help@mide.com',
         description='Python API for accessing IDE data recordings',
