0563b36a3c04bbda71659bce16a7960c20c0f5a2
David Stokes <dstokes@mide.com>
2022-06-09 18:32:07 -0400

    Added schemata directory to `ebmlite.SCHEMA_PATH`

--- idelib/__init__.py
+++ idelib/__init__.py
@@ -5,12 +5,19 @@ files.
 """
 
 __author__ = "David Randall Stokes"
-__copyright__ = "Copyright (c) 2021 Midé Technology"
+__copyright__ = "Copyright (c) 2022 Midé Technology"
 
-__maintainer__ = "Mide Technology"
+__maintainer__ = "Midé Technology"
 __email__ = "help@mide.com"
 
 __version__ = (3, 2, 4)
 __status__ = "Production/Stable"
 
 from .importer import importFile
+
+# Add EBML schema path to ebmlite search paths
+import ebmlite
+
+SCHEMA_PATH = "{idelib}/schemata"
+if SCHEMA_PATH not in ebmlite.SCHEMA_PATH:
+    ebmlite.SCHEMA_PATH.insert(0, SCHEMA_PATH)
