805ad6f2fbab3e7dd25f1f2ceda017da5a537baa
stokesMIDE <dstokes@mide.com>
2025-03-11 14:38:04 -0400

    Revert "Remove py3.8; modified Sphinx options"
    This reverts commit 53fcba0f714c64629cd02a44de0f6438059920f0.

--- docs/conf.py
+++ docs/conf.py
@@ -92,15 +92,11 @@ html_favicon = '_static/endaq-favicon.ico'
 # documentation.
 #
 html_theme_options = {
-    "logo": {
-        "link": "index"
-    },
+    "logo_link": "index",
     "github_url": "https://github.com/MideTechnology/idelib",
     "twitter_url": "https://twitter.com/enDAQ_sensors",
     "collapse_navigation": True,
-    "analytics": {
-        "google_analytics_id": "G-E9QXH4H5LP",
-    }
+    "google_analytics_id": "G-E9QXH4H5LP",
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
